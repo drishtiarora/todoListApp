5a53ae81c84541ec9dcca00a35265e1ee77189b0
Drishti Arora <darora2@binghamton.edu>
2019-12-08 16:05:25 -0500

    App initialization and node and mongoDB setup

---
+++ server.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var app = express();
+var cors = require('cors');
+var bodyParser = require('body-parser');
+var mongoose = require('mongoose');
+var schema = mongoose.Schema;
+
+mongoose.Promise = require('q').Promise;
+var port = 3000;
+app.use(cors({
+    origin: 'http://localhost:4000'
+}));
+
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+
+mongoose.connect('mongodb://localhost:27017/todoList', function (err) {
+    if (err) {
+        console.log("Not connected to db" + err);
+    }
+    else {
+        console.log("Successfully connected");
+        app.listen(port, function () {
+            console.log("Running on port" + port);
+        });
+    }
+});
